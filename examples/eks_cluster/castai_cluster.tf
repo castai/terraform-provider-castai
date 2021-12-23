f5d8cd3ef19965f95e7f31928bd4b7cd70fbaf79
varnastadues <varnastadeus@gmail.com>
2021-12-23 13:18:45 +0200

    feat: introduce cluster_token

--- examples/eks_cluster/castai_cluster.tf
+++ examples/eks_cluster/castai_cluster.tf
@@ -32,6 +32,6 @@ resource "helm_release" "castai_agent" {
   }
   set_sensitive {
     name  = "apiKey"
-    value = castai_eks_cluster.my_castai_cluster.agent_token
+    value = castai_eks_cluster.my_castai_cluster.cluster_token
   }
 }
