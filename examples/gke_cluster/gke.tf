eae38a705299dfea59e9820b57fa31e8b052f526
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-04-13 00:58:42 +0300

    feat: gke examples

---
+++ examples/gke_cluster/gke.tf
@@ -0,0 +1,41 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  version                    = "20.0.0"
+  project_id                 = var.project_id
+  name                       = var.cluster_name
+  region                     = var.cluster_region
+  zones                      = var.cluster_zones
+  network                    = module.vpc.network_name
+  subnetwork                 = module.vpc.subnets_names[0]
+  ip_range_pods              = "10.20.0.0/16"
+  ip_range_services          = "10.30.0.0/24"
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+
+  node_pools = [
+    {
+      name               = "default-node-pool"
+      machine_type       = "e2-medium"
+      min_count          = 1
+      max_count          = 100
+      local_ssd_count    = 0
+      disk_size_gb       = 100
+      disk_type          = "pd-standard"
+      image_type         = "COS_CONTAINERD"
+      auto_repair        = true
+      auto_upgrade       = true
+      preemptible        = false
+      initial_node_count = 1
+    },
+  ]
+}
--- examples/gke_cluster/variables.tf
+++ examples/gke_cluster/variables.tf
@@ -3,3 +3,17 @@ variable "castai_api_token" {}
 variable "project_id" {}
 variable "cluster_region" {}
 variable "cluster_name" {}
+
+
+variable "cluster_zones" {
+  type        = list(string)
+  description = "The zones to create the cluster."
+
+  default = ["europe-west-1-a", "europe-west-1-b"]
+}
+
+variable "delete_nodes_on_disconnect" {
+  type        = bool
+  description = "Optional parameter, if set to true - CAST AI provisioned nodes will be deleted from cloud on cluster disconnection."
+  default     = false
+}
