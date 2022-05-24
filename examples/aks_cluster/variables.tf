de9525f393851131bde0b9e0cc9c03bb39566b7b
Matas <matas.k@cast.ai>
2022-05-24 16:48:38 +0300

    fix

--- examples/aks_cluster/variables.tf
+++ examples/aks_cluster/variables.tf
@@ -6,11 +6,9 @@ variable "castai_api_token" {
 variable "aks_cluster_name" {
   type        = string
   description = "Name of the AKS cluster, resources will be created for."
-  default = "matas-05-23-tf-test"
 }
 
 variable "aks_resource_group" {
   type        = string
   description = "Name of the AKS resource group that will be created for AKS cluster."
-  default = "matas-05-23-tf-test_group"
 }
