582782344d2c29f2b58534f11392b9e03aab8186
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-05-24 07:48:36 +0200

    chore: add configuration for kubectl provider

--- examples/eks_cluster_webshop/main.tf
+++ examples/eks_cluster_webshop/main.tf
@@ -26,6 +26,12 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster_auth" "eks" {
