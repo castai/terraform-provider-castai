2b021bd83396fab2e638dad8b0dd7fc06de41ecf
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-05-31 10:20:26 +0200

    fix: remove cluster from state if it has archived status (#74)

--- castai/resource_castai_gke_cluster_test.go
+++ castai/resource_castai_gke_cluster_test.go
@@ -84,3 +84,58 @@ ssh_public_key = key-123
 Tainted = false
 `, data.State().String())
 }
+
+func TestGKEClusterResourceReadContextArchived(t *testing.T) {
+	r := require.New(t)
+	mockctrl := gomock.NewController(t)
+	mockClient := mock_sdk.NewMockClientInterface(mockctrl)
+
+	ctx := context.Background()
+	provider := &ProviderConfig{
+		api: &sdk.ClientWithResponses{
+			ClientInterface: mockClient,
+		},
+	}
+
+	clusterId := "b6bfc074-a267-400f-b8f1-db0850c36gke"
+
+	body := io.NopCloser(bytes.NewReader([]byte(`{
+  "id": "b6bfc074-a267-400f-b8f1-db0850c36gk3",
+  "name": "gke-cluster",
+  "organizationId": "2836f775-aaaa-eeee-bbbb-3d3c29512GKE",
+  "credentialsId": "9b8d0456-177b-4a3d-b162-e68030d65GKE",
+  "createdAt": "2022-04-27T19:03:31.570829Z",
+  "region": {
+    "name": "eu-central-1",
+    "displayName": "EU (Frankfurt)"
+  },
+  "status": "archived",
+  "agentSnapshotReceivedAt": "2022-05-21T10:33:56.192020Z",
+  "agentStatus": "online",
+  "providerType": "gke",
+  "gke": {
+    "clusterName": "gke-cluster",
+    "region": "eu-central-1",
+	"location": "eu-central-1",
+	"projectId": "project-id"
+  },
+  "sshPublicKey": "key-123",
+  "clusterNameId": "gke-cluster-b6bfc074",
+  "private": true
+}`)))
+	mockClient.EXPECT().
+		ExternalClusterAPIGetCluster(gomock.Any(), clusterId).
+		Return(&http.Response{StatusCode: 200, Body: body, Header: map[string][]string{"Content-Type": {"json"}}}, nil)
+
+	resource := resourceCastaiGKECluster()
+
+	val := cty.ObjectVal(map[string]cty.Value{})
+	state := terraform.NewInstanceStateShimmedFromValue(val, 0)
+	state.ID = clusterId
+
+	data := resource.Data(state)
+	result := resource.ReadContext(ctx, data, provider)
+	r.Nil(result)
+	r.False(result.HasError())
+	r.Equal(`<not created>`, data.State().String())
+}
