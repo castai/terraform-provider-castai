ae507db05d986fb01bc5dd7c9bebbc3680f23a78
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-05-31 16:15:53 +0300

    chore: decrease node pool for GKE

--- e2e/tests/gke_cluster_zonal/gke.tf
+++ e2e/tests/gke_cluster_zonal/gke.tf
@@ -20,7 +20,7 @@ module "gke" {
       machine_type       = "e2-medium"
       node_locations     = var.cluster_zones[0]
       min_count          = 1
-      max_count          = 100
+      max_count          = 3
       local_ssd_count    = 0
       disk_size_gb       = 100
       disk_type          = "pd-standard"
