bd50bb898564e2a71df44996454acf65e6fe4fc4
varnastadues <varnastadeus@gmail.com>
2022-09-19 13:02:43 +0300

    chore: update examples

--- examples/eks_cluster_readonly/variables.tf
+++ examples/eks_cluster_readonly/variables.tf
@@ -1,11 +1,5 @@
 variable "castai_api_token" {}
 
-variable "aws_access_key_id" {}
-
-variable "aws_secret_access_key" {}
-
-variable "aws_account_id" {}
-
 variable "cluster_region" {}
 
 variable "cluster_name" {}
