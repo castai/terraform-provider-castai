35f7f06e00ff5aa382ddb7cb8dd7255c43602802
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2022-09-27 09:16:03 +0200

    fix(webshop): add prefix to  EBS CSI

--- examples/eks_cluster_webshop/eks.tf
+++ examples/eks_cluster_webshop/eks.tf
@@ -106,7 +106,7 @@ module "ebs_csi_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 4.21.1"
 
-  role_name             = "ebs-csi"
+  role_name             = "ebs-csi-${var.cluster_name}"
   attach_ebs_csi_policy = true
 
   oidc_providers = {
