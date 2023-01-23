ac29d02ce2fab3c063e9c25d59742f75e2f0229e
andrejatcastai <101807823+andrejatcastai@users.noreply.github.com>
2023-01-23 09:45:41 +0200

    Minor change to regenerate FOSSA (#133)
    Co-authored-by: Andrej Kislovskij <andrej.kislovskij@cast.ai>

--- castai/resource_node_configuration_test.go
+++ castai/resource_node_configuration_test.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
