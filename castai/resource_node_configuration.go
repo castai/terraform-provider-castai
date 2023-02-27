c7cf35e7de9c68553f0162d74f652ea1c90d602c
falsai <filipe@cast.ai>
2023-02-27 16:11:02 +0100

    fix test

--- castai/resource_node_configuration.go
+++ castai/resource_node_configuration.go
@@ -541,6 +541,9 @@ func toEKSConfig(obj map[string]interface{}) *sdk.NodeconfigV1EKSConfig {
 	if v, ok := obj["volume_throughput"].(int); ok && v != 0 {
 		out.VolumeThroughput = toPtr(int32(v))
 	}
+	if v, ok := obj["imds_v1"].(bool); ok {
+		out.ImdsV1 = toPtr(v)
+	}
 
 	return out
 }
@@ -570,6 +573,9 @@ func flattenEKSConfig(config *sdk.NodeconfigV1EKSConfig) []map[string]interface{
 	}
 	if v := config.VolumeThroughput; v != nil {
 		m["volume_throughput"] = *config.VolumeThroughput
+	}
+	if v := config.ImdsV1; v != nil {
+		m["imds_v1"] = *config.ImdsV1
 	}
 
 	return []map[string]interface{}{m}
