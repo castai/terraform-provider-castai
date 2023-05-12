1ec48a5f22e40dc2ec2e637786780c4bf9720945
Mangirdas <mangirdas@cast.ai>
2023-05-12 11:09:28 +0300

    update tf.vars to tf.vars.examples

--- examples/aks/aks_cluster_autoscaler_policies/castai.tf
+++ examples/aks/aks_cluster_autoscaler_policies/castai.tf
@@ -14,7 +14,7 @@ provider "helm" {
     host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
     client_certificate      = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+    cluster_ca_certificate  = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
   }
 }
 
