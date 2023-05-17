106471154fb0a731e338e76a4243d1a61641e604
julgircast <133015764+julgircast@users.noreply.github.com>
2023-05-17 11:02:07 +0300

    Pass cluster region to kubernetes and helm providers (#169)

--- examples/eks/eks_cluster_custom_iam/castai.tf
+++ examples/eks/eks_cluster_custom_iam/castai.tf
@@ -20,7 +20,7 @@ provider "helm" {
         api_version = "client.authentication.k8s.io/v1beta1"
         command     = "aws"
         # This requires the awscli to be installed locally where Terraform is executed.
-        args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+        args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.cluster_region]
       }
   }
 }
