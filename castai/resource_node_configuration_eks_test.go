95c276818c1b985d0c430ee6bc6df03075e3a6ed
Jan Sykora <jan@cast.ai>
2023-07-31 17:18:38 +0200

    test: Make subnet routable to the internet

--- castai/resource_node_configuration_eks_test.go
+++ castai/resource_node_configuration_eks_test.go
@@ -199,6 +199,33 @@ resource "aws_subnet" "test" {
   }
 }
 
+resource "aws_internet_gateway" "test" { 
+  vpc_id = aws_vpc.test.id
+  tags = { 
+    Name = %[1]q
+  }
+}
+
+resource "aws_route_table" "test" {
+  vpc_id = aws_vpc.test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test.id
+  }
+
+  tags = {
+    Name = %[1]q
+  }
+}
+
+resource "aws_route_table_association" "test" {
+  count = 2
+
+  subnet_id      = element(aws_subnet.test[*].id, count.index)
+  route_table_id = aws_route_table.test.id
+}
+
 resource "aws_security_group" "test" {
   name        = %[1]q
   vpc_id      = aws_vpc.test.id
