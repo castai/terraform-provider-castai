a80899a3df3175bb70fb48c9508b993a62178501
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2024-02-19 17:02:23 +0100

    remove retry

--- castai/resource_node_template_test.go
+++ castai/resource_node_template_test.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/samber/lo"
 
-	"github.com/cenkalti/backoff/v4"
 	"github.com/golang/mock/gomock"
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -583,23 +582,19 @@ func testAccCheckNodeTemplateDestroy(s *terraform.State) error {
 		id := rs.Primary.ID
 		clusterID := rs.Primary.Attributes["cluster_id"]
 
-		if err := backoff.Retry(func() error {
-			response, err := client.NodeTemplatesAPIListNodeTemplatesWithResponse(ctx, clusterID, &sdk.NodeTemplatesAPIListNodeTemplatesParams{IncludeDefault: lo.ToPtr(false)})
-			if err != nil {
-				return err
-			}
-			if response.StatusCode() == http.StatusNotFound {
-				return nil
-			}
-			if len(*response.JSON200.Items) == 0 {
-				// Should be no templates
-				return nil
-			}
-
-			return fmt.Errorf("node template %q still exists; %v", id, string(response.GetBody()))
-		}, backoff.NewExponentialBackOff()); err != nil {
+		response, err := client.NodeTemplatesAPIListNodeTemplatesWithResponse(ctx, clusterID, &sdk.NodeTemplatesAPIListNodeTemplatesParams{IncludeDefault: lo.ToPtr(false)})
+		if err != nil {
 			return err
 		}
+		if response.StatusCode() == http.StatusNotFound {
+			return nil
+		}
+		if len(*response.JSON200.Items) == 0 {
+			// Should be no templates
+			return nil
+		}
+
+		return fmt.Errorf("node template %q still exists; %v", id, string(response.GetBody()))
 	}
 
 	return nil
