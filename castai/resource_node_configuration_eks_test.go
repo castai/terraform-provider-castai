a9d43dc6fed65e05c576c816b1ef86c569d6e776
Valentyna Bukhalova <v.bukhalova@gmail.com>
2024-03-25 13:26:42 +0100

    testset

--- castai/resource_node_configuration_eks_test.go
+++ castai/resource_node_configuration_eks_test.go
@@ -47,8 +47,9 @@ func TestAccResourceNodeConfiguration_eks(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "eks.0.imds_v1", "true"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.imds_hop_limit", "3"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.volume_kms_key_arn", "arn:aws:kms:eu-central-1:012345:key/1d989ee1-59cd-4238-8018-79bae29d1109"),
-					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.arn", "arn:test"),
-					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.port", "80"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.0.arn", "arn:test"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.0.port", "80"),
 					resource.TestCheckResourceAttr(resourceName, "aks.#", "0"),
 					resource.TestCheckResourceAttr(resourceName, "kops.#", "0"),
 					resource.TestCheckResourceAttr(resourceName, "gke.#", "0"),
@@ -130,10 +131,10 @@ resource "castai_node_configuration" "test" {
     volume_kms_key_arn   = "arn:aws:kms:eu-central-1:012345:key/1d989ee1-59cd-4238-8018-79bae29d1109"
 	imds_v1				 = true
 	imds_hop_limit       = 3
-	target_group [{
-	  arn = "arn:test"
-      port = 80
-    }]
+	target_group {
+	  arn  = "arn:test"
+	  port = 80
+    }
   }
 }
 
