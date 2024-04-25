da34d45f4343dafdf573cf0073a1f630dd255f1a
Max Twardowski <max@cast.ai>
2024-04-25 17:16:45 +0200

    format tf

--- examples/resources/castai_commitments/resource.tf
+++ examples/resources/castai_commitments/resource.tf
@@ -2,12 +2,12 @@ resource "castai_commitments" "gcp_test" {
   gcp_cuds_json = file("./cuds.json")
   gcp_cud_configs = [
     {
-      match_region = "us-east4"
-      match_type = "COMPUTE_OPTIMIZED_C2D"
-      match_name = "test"
+      match_region   = "us-east4"
+      match_type     = "COMPUTE_OPTIMIZED_C2D"
+      match_name     = "test"
       prioritization = true
-      allowed_usage = 0.6
-      status = "Inactive"
+      allowed_usage  = 0.6
+      status         = "Inactive"
     }
   ]
 }
