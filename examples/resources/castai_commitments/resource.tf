3a44eee4a199a63b86b3bbba120938485e5bd400
Max Twardowski <max@cast.ai>
2024-04-25 15:19:17 +0200

    add usage examples

--- examples/resources/castai_commitments/resource.tf
+++ examples/resources/castai_commitments/resource.tf
@@ -1,6 +1,6 @@
 resource "castai_commitments" "gcp_test" {
   gcp_cuds_json = file("./cuds.json")
-  gcp_cud_configs = [
+  commitment_configs = [
     {
       matcher = {
         region = "us-east4"
@@ -10,6 +10,22 @@ resource "castai_commitments" "gcp_test" {
       prioritization = true
       allowed_usage = 0.6
       status = "Inactive"
+    }
+  ]
+}
+
+resource "castai_commitments" "azure_test" {
+  azure_reservations_csv = file("./reservations.csv")
+  commitment_configs = [
+    {
+      matcher = {
+        region = "eastus"
+        type = "Standard_D32as_v4"
+        name = "test-res-1"
+      }
+      prioritization = false
+      allowed_usage = 0.9
+      status = "Active"
     }
   ]
 }
