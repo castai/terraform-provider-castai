ae63fe67fcc600860e5ed23e58dd195d763b8db8
Max Twardowski <max@cast.ai>
2024-04-26 17:06:18 +0200

    azure assignments acc tests

--- castai/resource_commitments_test.go
+++ castai/resource_commitments_test.go
@@ -54,6 +54,14 @@ func TestCommitments(t *testing.T) {
 				Source:            "hashicorp/google-beta",
 				VersionConstraint: "> 4.75.0",
 			},
+			"azurerm": {
+				Source:            "hashicorp/azurerm",
+				VersionConstraint: "~> 3.7.0",
+			},
+			"azuread": {
+				Source:            "hashicorp/azuread",
+				VersionConstraint: "~> 2.22.0",
+			},
 		},
 		Steps: []resource.TestStep{
 			{ // Import 2 commitments - one GCP CUD and one Azure reservations, both without configs
@@ -79,7 +87,7 @@ func TestCommitments(t *testing.T) {
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.start_timestamp", "2023-01-11T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.end_timestamp", "2050-01-01T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.name", "test-res-1"),
-					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.region", "eastus"),
+					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.region", "westeurope"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.plan", "THREE_YEAR"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.instance_type", "Standard_D32as_v4"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.count", "3"),
@@ -131,7 +139,7 @@ func TestCommitments(t *testing.T) {
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.start_timestamp", "2023-01-11T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.end_timestamp", "2050-01-01T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.name", "test-res-1"),
-					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.region", "eastus"),
+					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.region", "westeurope"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.plan", "THREE_YEAR"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.instance_type", "Standard_D32as_v4"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.count", "3"),
@@ -184,7 +192,7 @@ func TestCommitments(t *testing.T) {
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.start_timestamp", "2023-01-11T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.end_timestamp", "2050-01-01T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.name", "test-res-1"),
-					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.region", "eastus"),
+					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.region", "westeurope"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.plan", "THREE_YEAR"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.instance_type", "Standard_D32as_v4"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.count", "3"),
@@ -194,13 +202,14 @@ func TestCommitments(t *testing.T) {
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.prioritization", "false"),
 					checkFloatAttr("castai_commitments.test_azure", "azure_reservations.0.allowed_usage", 0.6),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.status", "Active"),
+					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.0.assignments.#", "1"),
 					// Azure - "test-res-2", added in the update with config
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.reservation_id", "3b3de39c-bc44-4d69-be2d-69527dfe9959"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.reservation_status", "Succeeded"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.start_timestamp", "2023-01-12T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.end_timestamp", "2040-01-01T00:00:00Z"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.name", "test-res-2"),
-					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.region", "eastus"),
+					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.region", "westeurope"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.plan", "ONE_YEAR"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.instance_type", "Standard_B1s"),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.count", "2"),
@@ -210,6 +219,7 @@ func TestCommitments(t *testing.T) {
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.prioritization", "true"),
 					checkFloatAttr("castai_commitments.test_azure", "azure_reservations.1.allowed_usage", 0.9),
 					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.status", "Active"),
+					resource.TestCheckResourceAttr("castai_commitments.test_azure", "azure_reservations.1.assignments.#", "1"),
 				),
 			},
 			importCUDsStateStep,
@@ -337,6 +347,11 @@ var (
 	clusterName = "max-04-26"
 	projectID   = os.Getenv("GOOGLE_PROJECT_ID")
 
+	rNameAzure            = fmt.Sprintf("%v-aks-%v", ResourcePrefix, acctest.RandString(8))
+	clusterNameAzure      = "core-tf-acc"
+	resourceGroupName     = "core-tf-acc"
+	nodeResourceGroupName = "core-tf-acc-ng"
+
 	commitmentsConfig1 = ConfigCompose(testAccGKEClusterConfig(rName, clusterName, projectID), `
 resource "castai_commitments" "test_gcp" {
 	gcp_cuds_json = <<EOF
@@ -349,12 +364,15 @@ resource "castai_commitments" "test_gcp" {
 resource "castai_commitments" "test_azure" {
 	azure_reservations_csv = <<EOF
 Name,Reservation Id,Reservation order Id,Status,Expiration date,Purchase date,Term,Scope,Scope subscription,Scope resource group,Type,Product name,Region,Quantity,Utilization % 1 Day,Utilization % 7 Day,Utilization % 30 Day,Deep link to reservation
-test-res-1,3b3de39c-bc44-4d69-be2d-69527dfe9958,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2050-01-01T00:00:00Z,2023-01-11T00:00:00Z,P3Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_D32as_v4,eastus,3,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
+test-res-1,3b3de39c-bc44-4d69-be2d-69527dfe9958,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2050-01-01T00:00:00Z,2023-01-11T00:00:00Z,P3Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_D32as_v4,westeurope,3,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
 	EOF
 }
 `)
 
-	commitmentsConfig2 = ConfigCompose(testAccGKEClusterConfig(rName, clusterName, projectID), `
+	commitmentsConfig2 = ConfigCompose(
+		testAccGKEClusterConfig(rName, clusterName, projectID),
+		testAccAKSClusterConfig(rNameAzure, clusterNameAzure, resourceGroupName, nodeResourceGroupName),
+		`
 resource "castai_commitments" "test_gcp" {
 	gcp_cuds_json = <<EOF
 [
@@ -392,12 +410,15 @@ resource "castai_commitments" "test_gcp" {
 resource "castai_commitments" "test_azure" {
 	azure_reservations_csv = <<EOF
 Name,Reservation Id,Reservation order Id,Status,Expiration date,Purchase date,Term,Scope,Scope subscription,Scope resource group,Type,Product name,Region,Quantity,Utilization % 1 Day,Utilization % 7 Day,Utilization % 30 Day,Deep link to reservation
-test-res-1,3b3de39c-bc44-4d69-be2d-69527dfe9958,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2050-01-01T00:00:00Z,2023-01-11T00:00:00Z,P3Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_D32as_v4,eastus,3,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
+test-res-1,3b3de39c-bc44-4d69-be2d-69527dfe9958,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2050-01-01T00:00:00Z,2023-01-11T00:00:00Z,P3Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_D32as_v4,westeurope,3,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
 	EOF
 }
 `)
 
-	commitmentsConfig3 = ConfigCompose(testAccGKEClusterConfig(rName, clusterName, projectID), `
+	commitmentsConfig3 = ConfigCompose(
+		testAccGKEClusterConfig(rName, clusterName, projectID),
+		testAccAKSClusterConfig(rNameAzure, clusterNameAzure, resourceGroupName, nodeResourceGroupName),
+		`
 resource "castai_commitments" "test_gcp" {
 	gcp_cuds_json = <<EOF
 [
@@ -438,35 +459,45 @@ resource "castai_commitments" "test_gcp" {
 resource "castai_commitments" "test_azure" {
 	azure_reservations_csv = <<EOF
 Name,Reservation Id,Reservation order Id,Status,Expiration date,Purchase date,Term,Scope,Scope subscription,Scope resource group,Type,Product name,Region,Quantity,Utilization % 1 Day,Utilization % 7 Day,Utilization % 30 Day,Deep link to reservation
-test-res-1,3b3de39c-bc44-4d69-be2d-69527dfe9958,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2050-01-01T00:00:00Z,2023-01-11T00:00:00Z,P3Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_D32as_v4,eastus,3,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
-test-res-2,3b3de39c-bc44-4d69-be2d-69527dfe9959,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2040-01-01T00:00:00Z,2023-01-12T00:00:00Z,P1Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_B1s,eastus,2,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
+test-res-1,3b3de39c-bc44-4d69-be2d-69527dfe9958,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2050-01-01T00:00:00Z,2023-01-11T00:00:00Z,P3Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_D32as_v4,westeurope,3,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
+test-res-2,3b3de39c-bc44-4d69-be2d-69527dfe9959,630226bb-5170-4b95-90b0-f222757130c1,Succeeded,2040-01-01T00:00:00Z,2023-01-12T00:00:00Z,P1Y,Single subscription,8faa0959-093b-4612-8686-a996ac19db00,All resource groups,VirtualMachines,Standard_B1s,westeurope,2,100,100,100,https://portal.azure.com#resource/providers/microsoft.capacity/reservationOrders/59791a62-264b-4b9f-aa3a-5eeb761e4583/reservations/883afd52-54c8-4bc6-a0f2-ccbaf7b84bda/overview
 	EOF
 
 	commitment_configs {
 		matcher {
 			name = "test-res-1"
-			region = "eastus"
+			region = "westeurope"
 			type = "Standard_D32as_v4"
 		}
 		prioritization = false
 		allowed_usage = 0.6
 		status = "Active"
+		assignments {
+			cluster_id = castai_aks_cluster.test.id
+		}
 	}
 
 	commitment_configs {
 		matcher {
 			name = "test-res-2"
-			region = "eastus"
+			region = "westeurope"
 			type = "Standard_B1s"
 		}
 		prioritization = true
 		allowed_usage = 0.9
 		status = "Active"
+		assignments {
+			cluster_id = castai_aks_cluster.test.id
+		}
 	}
 }
 `)
 
 	commitmentsConfig4 = ConfigCompose(testAccGKEClusterConfig(rName, clusterName, projectID), `
+provider "azurerm" {
+  features {}
+}
+
 resource "castai_commitments" "test_gcp" {
 	gcp_cuds_json = <<EOF
 [
@@ -506,6 +537,10 @@ resource "castai_commitments" "test_gcp" {
 `)
 
 	commitmentsConfig5 = ConfigCompose(testAccGKEClusterConfig(rName, clusterName, projectID), `
+provider "azurerm" {
+  features {}
+}
+
 resource "castai_commitments" "test_gcp" {
 	gcp_cuds_json = <<EOF
 [
