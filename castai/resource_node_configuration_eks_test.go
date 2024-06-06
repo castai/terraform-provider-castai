dc16429ad80f2acefc1d1e340082762c71e9e296
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2024-06-06 15:55:38 +0200

    fix test

--- castai/resource_node_configuration_eks_test.go
+++ castai/resource_node_configuration_eks_test.go
@@ -44,12 +44,12 @@ func TestAccResourceNodeConfiguration_eks(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "eks.0.volume_type", "gp3"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.volume_iops", "3100"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.volume_throughput", "131"),
-					resource.TestCheckResourceAttr(resourceName, "eks.0.max_pods_per_node_formula", "NUM_IP_PER_PREFIX+NUM_MAX_NET_INTERFACES"),
-					resource.TestCheckResourceAttr(resourceName, "eks.0.ips_per_prefix", "3"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.imds_v1", "true"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.imds_hop_limit", "3"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.volume_kms_key_arn", "arn:aws:kms:eu-central-1:012345:key/1d989ee1-59cd-4238-8018-79bae29d1109"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.max_pods_per_node_formula", "NUM_IP_PER_PREFIX+NUM_MAX_NET_INTERFACES"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.ips_per_prefix", "3"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.0.arn", "arn:aws:test"),
 					resource.TestCheckResourceAttr(resourceName, "aks.#", "0"),
 					resource.TestCheckResourceAttr(resourceName, "kops.#", "0"),
@@ -80,6 +80,8 @@ func TestAccResourceNodeConfiguration_eks(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "eks.0.dns_cluster_ip", ""),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.security_groups.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.volume_throughput", "130"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.max_pods_per_node_formula", "NUM_IP_PER_PREFIX-NUM_MAX_NET_INTERFACES"),
+					resource.TestCheckResourceAttr(resourceName, "eks.0.ips_per_prefix", "4"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.0.arn", "arn:aws:test2"),
 					resource.TestCheckResourceAttr(resourceName, "eks.0.target_group.0.port", "80"),
@@ -134,6 +136,8 @@ resource "castai_node_configuration" "test" {
     volume_kms_key_arn   = "arn:aws:kms:eu-central-1:012345:key/1d989ee1-59cd-4238-8018-79bae29d1109"
 	imds_v1				 = true
 	imds_hop_limit       = 3
+    max_pods_per_node_formula = "NUM_IP_PER_PREFIX-NUM_MAX_NET_INTERFACES"
+	ips_per_prefix = 4
 	target_group 	     {
 	   arn = "arn:aws:test"
     }
@@ -163,8 +167,8 @@ resource "castai_node_configuration" "test" {
 	instance_profile_arn = aws_iam_instance_profile.test.arn
     security_groups      = [aws_security_group.test.id]
     volume_throughput 	 = 130
-    max_pods_per_node_formula = "NUM_IP_PER_PREFIX+NUM_MAX_NET_INTERFACES"
-	ips_per_prefix = 3
+    max_pods_per_node_formula = "NUM_IP_PER_PREFIX-NUM_MAX_NET_INTERFACES"
+	ips_per_prefix = 4
     target_group 	     {
 	   arn = "arn:aws:test2"
        port = 80
