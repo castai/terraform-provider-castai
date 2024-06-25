5c8585d0b8bd9101cb00bb79f92914e759190a1e
Ali AKCA <ali@akca.io>
2024-06-25 09:25:02 +0200

    gen: update generated resources

--- castai/sdk/client.gen.go
+++ castai/sdk/client.gen.go
@@ -4066,6 +4066,22 @@ func NewExternalClusterAPIGetCredentialsScriptRequest(server string, clusterId s
 
 	}
 
+	if params.InstallGpuMetricsExporter != nil {
+
+		if queryFrag, err := runtime.StyleParamWithLocation("form", true, "installGpuMetricsExporter", runtime.ParamLocationQuery, *params.InstallGpuMetricsExporter); err != nil {
+			return nil, err
+		} else if parsed, err := url.ParseQuery(queryFrag); err != nil {
+			return nil, err
+		} else {
+			for k, v := range parsed {
+				for _, v2 := range v {
+					queryValues.Add(k, v2)
+				}
+			}
+		}
+
+	}
+
 	queryURL.RawQuery = queryValues.Encode()
 
 	req, err := http.NewRequest("GET", queryURL.String(), nil)
