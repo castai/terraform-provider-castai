1d5f5009e00aff0538fe25fe67c5e2062c87e114
sarvesh-cast <sarvesh@cast.ai>
2024-09-30 09:07:24 +0530

    Update GKE examples (#384)

--- examples/gke/gke_cluster_gitops/castai.tf
+++ examples/gke/gke_cluster_gitops/castai.tf
@@ -7,18 +7,6 @@ resource "castai_gke_cluster" "this" {
   credentials_json = module.castai-gke-iam.private_key
 }
 
-resource "castai_node_configuration" "default" {
-  cluster_id     = castai_gke_cluster.this.id
-  name           = "default"
-  disk_cpu_ratio = 0
-  min_disk_size  = 100
-  subnets        = var.subnets
-
-  gke {
-    disk_type = "pd-standard"
-  }
-}
-
 module "castai-gke-iam" {
   source = "castai/gke-iam/castai"
 
@@ -26,7 +14,114 @@ module "castai-gke-iam" {
   gke_cluster_name = var.cluster_name
 }
 
+resource "castai_node_configuration" "default" {
+  cluster_id     = castai_gke_cluster.this.id
+  name           = "default"
+  disk_cpu_ratio = 0
+  min_disk_size  = 100
+  subnets        = var.subnets
+}
+
 resource "castai_node_configuration_default" "this" {
   cluster_id       = castai_gke_cluster.this.id
   configuration_id = castai_node_configuration.default.id
+}
+
+resource "castai_node_template" "default_by_castai" {
+  cluster_id = castai_gke_cluster.this.id
+
+  name             = "default-by-castai"
+  is_default       = true
+  is_enabled       = true
+  configuration_id = castai_node_configuration.default.id
+  should_taint     = false
+
+  constraints {
+    on_demand = true
+  }
+}
+
+resource "castai_node_template" "example_spot_template" {
+  cluster_id = castai_gke_cluster.this.id
+
+  name                     = "example_spot_template"
+  is_default               = false
+  is_enabled               = true
+  configuration_id         = castai_node_configuration.default.id
+  should_taint             = true
+  custom_instances_enabled = true # gke specific
+
+  custom_labels = {
+    type = "spot"
+  }
+
+  custom_taints {
+    key    = "dedicated"
+    value  = "backend"
+    effect = "NoSchedule"
+  }
+
+  constraints {
+    spot                                        = true
+    use_spot_fallbacks                          = true
+    fallback_restore_rate_seconds               = 1800
+    enable_spot_diversity                       = true
+    spot_diversity_price_increase_limit_percent = 20
+    min_cpu                                     = 2
+    max_cpu                                     = 8
+    min_memory                                  = 4096
+    max_memory                                  = 16384
+    architectures                               = ["amd64"]
+    burstable_instances                         = "disabled"
+    customer_specific                           = "enabled"
+
+    instance_families {
+      exclude = ["e2"]
+    }
+    custom_priority {
+      instance_families = ["c4"]
+      spot              = true
+    }
+  }
+
+}
+
+resource "castai_autoscaler" "castai_autoscaler_policy" {
+  cluster_id = castai_gke_cluster.this.id
+
+  autoscaler_settings {
+    enabled                                 = true
+    is_scoped_mode                          = false
+    node_templates_partial_matching_enabled = false
+
+    unschedulable_pods {
+      enabled = true
+    }
+
+    cluster_limits {
+      enabled = false
+
+      cpu {
+        min_cores = 1
+        max_cores = 200
+      }
+    }
+
+    node_downscaler {
+      enabled = true
+
+      empty_nodes {
+        enabled = true
+      }
+
+      evictor {
+        aggressive_mode           = false
+        cycle_interval            = "60s"
+        dry_run                   = false
+        enabled                   = false
+        node_grace_period_minutes = 10
+        scoped_mode               = false
+      }
+    }
+  }
 }
