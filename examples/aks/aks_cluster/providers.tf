3f8137b615442f8d3c03444bb8619057975d7a7b
Marcin Kaciuba <marcin.kaciuba@gmail.com>
2024-10-15 12:12:30 +0200

    chore: fix example for AKS

--- examples/aks/aks_cluster/providers.tf
+++ examples/aks/aks_cluster/providers.tf
@@ -1,9 +1,9 @@
 # Following providers required by AKS and Vnet resources.
 provider "azurerm" {
+  subscription_id = var.subscription_id
   features {}
 }
 
 provider "azuread" {
-  version   = "2.53.1"
   tenant_id = data.azurerm_subscription.current.tenant_id
 }
