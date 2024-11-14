a97dc2b0f7c46c2b1d8c16f0bf171bfea67f4f79
Andrej Kislovskij <101807823+andrejatcastai@users.noreply.github.com>
2024-11-14 12:23:06 +0200

    NOJIRA: Add IPv6 enabled EKS cluster to the examples (#420)

---
+++ examples/eks/eks_cluster_ipv6/vpc.tf
@@ -0,0 +1,41 @@
+# 1. Create VPC.
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = var.cluster_name
+  region = var.cluster_region
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 4)]
+
+  enable_nat_gateway                             = true
+  enable_ipv6                                    = true
+  public_subnet_assign_ipv6_address_on_creation  = true
+  private_subnet_assign_ipv6_address_on_creation = true
+
+  public_subnet_ipv6_prefixes  = [0, 1, 2]
+  private_subnet_ipv6_prefixes = [3, 4, 5]
+
+  tags = local.tags
+}
