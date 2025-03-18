7111dc954470a88e3dfd1fb6b2b7dccc47ed6b7f
cirisked <illia@cast.ai>
2025-03-18 15:14:15 +0000

    CSU-2970

--- castai/sdk/client.go
+++ castai/sdk/client.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
 )
 
@@ -23,8 +24,13 @@ const (
 
 func CreateClient(apiURL, apiToken, userAgent string) (*ClientWithResponses, error) {
 	httpClientOption := func(client *Client) error {
+		retries := retryablehttp.NewClient()
+		retries.RetryMax = 2
+
+		c := retries.StandardClient()
+
 		client.Client = &http.Client{
-			Transport: logging.NewSubsystemLoggingHTTPTransport("CAST.AI", http.DefaultTransport),
+			Transport: logging.NewSubsystemLoggingHTTPTransport("CAST.AI", c.Transport),
 			Timeout:   1 * time.Minute,
 		}
 		client.RequestEditors = append(client.RequestEditors, func(_ context.Context, req *http.Request) error {
