5e50e2f124f41a5740dc7434d1119652af6ae621
juliette-cast <juliette@cast.ai>
2025-04-15 05:55:49 -0400

    Adding cast anywhere TF config (#476)
    * Adding cast anywhere TF config 
    
    This config installs all the components that the cast anywhere script installs via the UI. tested and it works with no issues
    
    * Update main.tf
    
    * Update variables.tf

---
+++ examples/castai-anywhere-terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "cast_ai_api_key" {
+  description = "Your CAST AI API key"
+  type        = string
+  sensitive   = true
+  default     = "" # add your api key
+}
+
+
+
+variable "cluster_name" {
+  description = "Name of your cluster"
+  type        = string
+  default     = "" # give a name for your cluster
+}
+
+variable "cluster_id" {
+  description = "Identifier for your CAST AI cluster"
+  type        = string
+  default     = "" # add the cluster ID you copied from the UI from the 1st apply
+}
+
+variable "organization_id" {
+  description = "Your CAST AI Organization ID"
+  type        = string
+}
+
+variable "managed_by_castai" {
+  description = "Flag to indicate if the components are managed by CAST AI"
+  type        = bool
+  default     = false # if its true, CAST overrides every changes made 
+}
