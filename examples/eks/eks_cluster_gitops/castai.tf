7891dfae22f551866dec0358f4f824f11f903669
juliette-cast <juliette@cast.ai>
2025-04-16 10:02:05 -0400

    Update castai.tf

--- examples/eks/eks_cluster_gitops/castai.tf
+++ examples/eks/eks_cluster_gitops/castai.tf
@@ -123,7 +123,7 @@ resource "castai_node_template" "example_spot_template" {
     min_memory                                  = 4096
     max_memory                                  = 24576
     architectures                               = ["amd64"]
-    azs                                         = ["", ""] 
+    azs                                         = [] 
     customer_specific                           = "disabled"
 
     instance_families {
