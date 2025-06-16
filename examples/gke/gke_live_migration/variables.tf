4944f14b9fda7fb5ace122a1e4ce2245f8d7ce3d
Filipe Augusto Lima de Souza <fals.xp@gmail.com>
2025-06-16 17:35:22 +0200

    adding live-migration examples

---
+++ examples/gke/gke_live_migration/variables.tf
@@ -0,0 +1,56 @@
+# GKE module variables.
+variable "cluster_name" {
+  type        = string
+  description = "GKE cluster name in GCP project."
+}
+
+variable "cluster_region" {
+  type        = string
+  description = "The region to create the cluster."
+}
+
+variable "cluster_zones" {
+  type        = list(string)
+  description = "The zones to create the cluster."
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID in which GKE cluster would be created."
+}
+
+variable "castai_api_url" {
+  type        = string
+  description = "URL of alternative CAST AI API to be used during development or testing"
+  default     = "https://api.cast.ai"
+}
+
+# Variables required for connecting EKS cluster to CAST AI
+variable "castai_api_token" {
+  type        = string
+  description = "CAST AI API token created in console.cast.ai API Access keys section."
+}
+
+variable "castai_grpc_url" {
+  type        = string
+  description = "CAST AI gRPC URL"
+  default     = "grpc.cast.ai:443"
+}
+
+variable "delete_nodes_on_disconnect" {
+  type        = bool
+  description = "Optional parameter, if set to true - CAST AI provisioned nodes will be deleted from cloud on cluster disconnection. For production use it is recommended to set it to false."
+  default     = true
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Optional tags for new cluster nodes. This parameter applies only to new nodes - tags for old nodes are not reconciled."
+  default     = {}
+}
+
+variable "gke_img_type" {
+  type        = string
+  description = "defines the type of image cluster nodes should use."
+  default     = "COS_CONTAINERD"
+}
