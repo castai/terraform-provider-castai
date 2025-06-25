bf49824cf47c7fb7b82d3df3aabadfaac05a5235
juliette-cast <juliette@cast.ai>
2025-06-25 17:23:09 -0400

    Update providers.tf

--- examples/eks/eks_cluster_with_security_runtime_rules/providers.tf
+++ examples/eks/eks_cluster_with_security_runtime_rules/providers.tf
@@ -21,14 +21,14 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed.
-      args = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.cluster_region, "--profile", var.profile]
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.cluster_region, "--profile", var.profile]
     }
   }
 }
+
