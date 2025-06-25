2aac0c22b4078fa2077f592fbbf7f229bc8d618f
juliette-cast <juliette@cast.ai>
2025-06-25 16:59:38 -0400

    Update castai.tf
    updating Because helm expects the kubernetes config to be passed as a map, not a nested block.

--- examples/eks/eks_cluster_custom_iam/castai.tf
+++ examples/eks/eks_cluster_custom_iam/castai.tf
@@ -13,14 +13,13 @@ provider "castai" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed.
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.cluster_region]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.cluster_region]
     }
   }
 }
