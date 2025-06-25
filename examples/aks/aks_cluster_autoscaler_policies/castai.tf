9cb1833724b9eed064efef764336bbd9fecbcc38
juliette-cast <juliette@cast.ai>
2025-06-25 17:16:30 -0400

    Update castai.tf

--- examples/aks/aks_cluster_autoscaler_policies/castai.tf
+++ examples/aks/aks_cluster_autoscaler_policies/castai.tf
@@ -9,13 +9,14 @@ provider "castai" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
   }
 }
+
 
 # Configure AKS cluster connection to CAST AI using CAST AI aks-cluster module.
 module "castai-aks-cluster" {
