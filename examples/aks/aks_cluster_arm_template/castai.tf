34496c0632df8007dc35baecc97829595b957302
juliette-cast <juliette@cast.ai>
2025-06-25 17:44:29 -0400

    Update castai.tf

--- examples/aks/aks_cluster_arm_template/castai.tf
+++ examples/aks/aks_cluster_arm_template/castai.tf
@@ -10,12 +10,13 @@ provider "castai" {
 
 provider "helm" {
   kubernetes = {
-    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
   }
 }
+
 
 
 # Configure AKS cluster connection to CAST AI using CAST AI aks-cluster module.
