75aabc1c38fd2d543231807b2d41bc2673f55999
juliette-cast <juliette@cast.ai>
2025-06-25 17:28:07 -0400

    Update providers.tf

--- examples/aks/aks_cluster_existing/providers.tf
+++ examples/aks/aks_cluster_existing/providers.tf
@@ -14,10 +14,11 @@ provider "castai" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.example.kube_config.0.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)
+  kubernetes = {
+    host                   = data.azurerm_kubernetes_cluster.example.kube_config[0].host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.example.kube_config[0].client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.example.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.example.kube_config[0].cluster_ca_certificate)
   }
 }
+
