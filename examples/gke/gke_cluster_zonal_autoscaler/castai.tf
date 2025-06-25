7e4a9d4ddc4f59ab871ddae3897b7b601c369883
juliette-cast <juliette@cast.ai>
2025-06-25 17:34:48 -0400

    Update castai.tf

--- examples/gke/gke_cluster_zonal_autoscaler/castai.tf
+++ examples/gke/gke_cluster_zonal_autoscaler/castai.tf
@@ -10,12 +10,13 @@ provider "castai" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = "https://${module.gke.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(module.gke.ca_certificate)
   }
 }
+
 
 # Configure GKE cluster connection using CAST AI gke-cluster module.
 module "castai-gke-iam" {
