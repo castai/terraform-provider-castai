e993f56beb892c9487ec11aaae68469143296345
juliette-cast <juliette@cast.ai>
2025-06-25 17:57:31 -0400

    Update providers.tf

--- examples/eks/eks_cluster_existing/providers.tf
+++ examples/eks/eks_cluster_existing/providers.tf
@@ -11,12 +11,16 @@ provider "castai" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.existing_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.existing_cluster.certificate_authority.0.data)
-  exec {
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.existing_cluster.certificate_authority[0].data)
+  exec = {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", var.cluster_name, "--region", var.cluster_region, "--profile", var.profile]
+    args = [
+      "eks", "get-token",
+      "--cluster-name", var.cluster_name,
+      "--region", var.cluster_region,
+      "--profile", var.profile
+    ]
   }
 }
 
@@ -27,7 +31,7 @@ provider "helm" {
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = [
+      args = [
         "eks", "get-token",
         "--cluster-name", var.cluster_name,
         "--region", var.cluster_region,
@@ -36,4 +40,5 @@ provider "helm" {
     }
   }
 }
+
 
