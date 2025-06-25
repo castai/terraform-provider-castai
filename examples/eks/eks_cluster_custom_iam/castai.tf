49e42540f62f94cdfcaf8e2723da194fe7395127
juliette-cast <juliette@cast.ai>
2025-06-25 17:56:25 -0400

    Update castai.tf

--- examples/eks/eks_cluster_custom_iam/castai.tf
+++ examples/eks/eks_cluster_custom_iam/castai.tf
@@ -19,10 +19,15 @@ provider "helm" {
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.cluster_region]
+      args = [
+        "eks", "get-token",
+        "--cluster-name", module.eks.cluster_name,
+        "--region", var.cluster_region
+      ]
     }
   }
 }
+
 
 
 
