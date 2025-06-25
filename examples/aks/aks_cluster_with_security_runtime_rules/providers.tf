ac13a87910fa43aa74a8b221ddad27d2060aa022
juliette-cast <juliette@cast.ai>
2025-06-25 17:31:15 -0400

    Update providers.tf

--- examples/aks/aks_cluster_with_security_runtime_rules/providers.tf
+++ examples/aks/aks_cluster_with_security_runtime_rules/providers.tf
@@ -14,10 +14,11 @@ provider "azuread" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
   }
 }
+
