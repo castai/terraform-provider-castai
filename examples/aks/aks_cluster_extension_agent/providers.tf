a7f7f0113202dbdcff2eb92f18a4ee8010233680
juliette-cast <juliette@cast.ai>
2025-06-25 17:48:28 -0400

    Update providers.tf

--- examples/aks/aks_cluster_extension_agent/providers.tf
+++ examples/aks/aks_cluster_extension_agent/providers.tf
@@ -8,15 +8,16 @@ provider "azuread" {
 }
 
 provider "castai" {
-  api_url   = var.castai_api_url
   api_token = var.castai_api_token
+  api_url   = var.castai_api_url
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
   }
 }
+
