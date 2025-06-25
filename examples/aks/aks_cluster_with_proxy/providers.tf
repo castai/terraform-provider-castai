6ada136f256937c8b3148bf65e27e24a07bb022e
juliette-cast <juliette@cast.ai>
2025-06-25 17:49:27 -0400

    Update providers.tf

--- examples/aks/aks_cluster_with_proxy/providers.tf
+++ examples/aks/aks_cluster_with_proxy/providers.tf
@@ -8,15 +8,15 @@ provider "azuread" {
 }
 
 provider "castai" {
-  api_url   = var.castai_api_url
   api_token = var.castai_api_token
+  api_url   = var.castai_api_url
 }
 
 provider "helm" {
-  kubernetes = {
-    host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
   }
 }
