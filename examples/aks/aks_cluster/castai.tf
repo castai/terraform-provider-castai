7cd725e69b8d5cf3c78c3e4fdda19b4ad9446350
juliette-cast <juliette@cast.ai>
2025-06-26 10:02:43 -0400

    Update castai.tf

--- examples/aks/aks_cluster/castai.tf
+++ examples/aks/aks_cluster/castai.tf
@@ -4,16 +4,16 @@
 data "azurerm_subscription" "current" {}
 
 provider "castai" {
-  api_token = var.castai_api_token
   api_url   = var.castai_api_url
+  api_token = var.castai_api_token
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+  kubernetes = {
+    host                   = azurerm_kubernetes_cluster.this.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config[0].cluster_ca_certificate)
   }
 }
 
