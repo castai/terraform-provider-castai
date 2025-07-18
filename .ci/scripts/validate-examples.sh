221245862e3c68dbfb4e01debce913042348de32
Daniel Voros <daniel.voros@cast.ai>
2025-07-18 13:00:13 +0200

    chore(ci): only run with -upgrade if failed without it
    Should help speed things up when running locally.

--- .ci/scripts/validate-examples.sh
+++ .ci/scripts/validate-examples.sh
@@ -7,6 +7,6 @@ PWD_ROOT="${PWD}"
 while IFS='' read -r -d $'\0' TFPROJECT; do
   echo "Validating ${PWD_ROOT}/$TFPROJECT"
   cd "${PWD_ROOT}/$TFPROJECT"
-  terraform init -upgrade
+  terraform init || terraform init -upgrade
   terraform validate
 done < <(find examples/eks examples/gke examples/aks -type d -depth 1 -print0)
