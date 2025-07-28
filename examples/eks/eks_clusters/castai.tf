b64ca10e646ac53a899e415dce23fba855189ab3
Damian Czaja <trojan295@protonmail.com>
2025-07-28 16:48:42 +0200

    NOJIRA: add depends_on to fix eks_clusters TF module example

--- examples/eks/eks_clusters/castai.tf
+++ examples/eks/eks_clusters/castai.tf
@@ -11,5 +11,6 @@ module "cluster" {
     module.eks.node_security_group_id,
     aws_security_group.additional.id,
   ]
-  subnets = module.vpc.private_subnets
+  subnets    = module.vpc.private_subnets
+  depends_on = [module.eks]
 }
