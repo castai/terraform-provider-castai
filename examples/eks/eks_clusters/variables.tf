7e0aa378cb3a56c4c44c945db7a522fdd33936dd
juliette-cast <juliette@cast.ai>
2025-08-04 02:32:14 -0400

    Updating K8s versions for terraform examples to more recent version (#547)
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version
    
    * Update k8s version

--- examples/eks/eks_clusters/variables.tf
+++ examples/eks/eks_clusters/variables.tf
@@ -12,7 +12,7 @@ variable "cluster_region" {
 variable "cluster_version" {
   type        = string
   description = "EKS cluster version."
-  default     = "1.28"
+  default     = "1.31"
 }
 
 variable "castai_api_url" {
