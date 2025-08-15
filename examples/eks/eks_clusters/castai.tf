194c5b05215a8c6b13bf6f91ff8878d0c290e524
kamalkailasa <kamal@cast.ai>
2025-08-15 17:54:42 +0530

    chore: fix Cluster is not ready after 10 minutes (#545)
    * chore: fix Cluster is not ready after 10 minutes
    
    * chore: fix Cluster is not ready after 10 minutes
    
    * chore: fix Cluster is not ready after 10 minutes
    
    * chore: fix Cluster is not ready after 10 minutes
    
    * chore: fix Cluster is not ready after 10 minutes
    
    * chore: fix Cluster is not ready after 10 minutes
    
    * chore: fix Cluster is not ready after 10 minutes

--- examples/eks/eks_clusters/castai.tf
+++ examples/eks/eks_clusters/castai.tf
@@ -2,7 +2,7 @@ module "cluster" {
   source = "./module/castai"
   count  = var.enable_castai ? 1 : 0
 
-  cluster_name     = var.cluster_name
+  cluster_name     = module.eks.cluster_name
   castai_api_token = var.castai_api_token
   cluster_region   = var.cluster_region
   vpc_id           = module.vpc.vpc_id
