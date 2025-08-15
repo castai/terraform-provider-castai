2fa3f81ff7a57753382715676583f5f17e64a49a
Daniel Voros <daniel.voros@cast.ai>
2025-08-15 14:33:58 +0200

    chore: bump helm to 3.x in gke examples

--- examples/gke/gke_cluster_existing/providers.tf
+++ examples/gke/gke_cluster_existing/providers.tf
@@ -4,7 +4,7 @@ provider "castai" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)
