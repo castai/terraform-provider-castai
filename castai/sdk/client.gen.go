6aadc5abdd680533707a4cc7617d68aa0a193301
Daniel Voros <daniel.voros@cast.ai>
2025-11-12 14:57:18 +0100

    chore: make generate-sdk

--- castai/sdk/client.gen.go
+++ castai/sdk/client.gen.go
@@ -8199,6 +8199,22 @@ func NewExternalClusterAPIGetCredentialsScriptRequest(server string, clusterId s
 
 		}
 
+		if params.InstallOperator != nil {
+
+			if queryFrag, err := runtime.StyleParamWithLocation("form", true, "installOperator", runtime.ParamLocationQuery, *params.InstallOperator); err != nil {
+				return nil, err
+			} else if parsed, err := url.ParseQuery(queryFrag); err != nil {
+				return nil, err
+			} else {
+				for k, v := range parsed {
+					for _, v2 := range v {
+						queryValues.Add(k, v2)
+					}
+				}
+			}
+
+		}
+
 		queryURL.RawQuery = queryValues.Encode()
 	}
 
