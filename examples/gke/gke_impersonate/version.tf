b2b8bf9661cb98342ddaa50e405796bda7fc93b6
varnastadeus <varnastadeus@gmail.com>
2025-11-13 12:17:25 +0200

    fix: rename oci private_key_wo -> private_key_base64_wo

--- examples/gke/gke_impersonate/version.tf
+++ examples/gke/gke_impersonate/version.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     castai = {
       source  = "castai/castai"
-      version = "~> 7.16"
+      version = ">= 8.1"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
