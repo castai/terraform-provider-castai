ea1408be974c807dd456c56d9286615798bb8185
varnastadeus <varnastadeus@gmail.com>
2025-11-18 12:50:43 +0200

    feat: add omni-cluster resource delete implementation

---
+++ castai/resource_omni_cluster_test.go
@@ -0,0 +1,76 @@
+package castai
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func TestAccOmniCluster_basic(t *testing.T) {
+	resourceName := "castai_omni_cluster.test"
+	clusterName := "omni-tf-acc-cluster"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		CheckDestroy:             testAccCheckOmniClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOmniClusterConfig(clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "id"),
+					resource.TestCheckResourceAttr(resourceName, "organization_id", testAccGetOrganizationID()),
+					resource.TestCheckResourceAttrSet(resourceName, "cluster_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOmniClusterDestroy(s *terraform.State) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	client := testAccProvider.Meta().(*ProviderConfig).omniAPI
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "castai_omni_cluster" {
+			continue
+		}
+
+		organizationID := rs.Primary.Attributes["organization_id"]
+		clusterID := rs.Primary.ID
+
+		response, err := client.ClustersAPIGetClusterWithResponse(ctx, organizationID, clusterID)
+		if err != nil {
+			return err
+		}
+		if response.StatusCode() == http.StatusNotFound {
+			return nil
+		}
+
+		return fmt.Errorf("omni cluster %s still exists", rs.Primary.ID)
+	}
+
+	return nil
+}
+
+func testAccOmniClusterConfig(clusterName string) string {
+	organizationID := testAccGetOrganizationID()
+	return fmt.Sprintf(`
+resource "castai_gke_cluster" "test" {
+  project_id = "test-project-123456"
+  location   = "us-central1-c"
+  name       = %[2]q
+}
+
+resource "castai_omni_cluster" "test" {
+  organization_id = %[1]q
+  cluster_id      = castai_gke_cluster.test.id
+}
+`, organizationID, clusterName)
+}
