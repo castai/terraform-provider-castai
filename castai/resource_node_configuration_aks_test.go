49f5548c708831eca477654627a74422d8ffe16b
Furkhat Kasymov Genii Uulu <vailodf@gmail.com>
2025-12-10 15:33:21 +0200

    chore: avoid re-onboarding a cluster in aks tests (#622)
    * avoid re-onboarding a cluster in aks tests
    
    * update api gen code
    
    ---------
    
    Co-authored-by: Furkhat Kasymov Genii Uulu <furkhat@cast.ai>

--- castai/resource_node_configuration_aks_test.go
+++ castai/resource_node_configuration_aks_test.go
@@ -2,75 +2,7 @@ package castai
 
 import (
 	"fmt"
-	"testing"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
-
-func TestAccAKS_ResourceNodeConfiguration(t *testing.T) {
-	rName := fmt.Sprintf("%v-node-cfg-aks-%v", ResourcePrefix, acctest.RandString(8))
-	resourceName := "castai_node_configuration.test"
-	clusterName := "core-tf-acc"
-	resourceGroupName := "core-tf-acc"
-	nodeResourceGroupName := "core-tf-acc-ng"
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		ProviderFactories: providerFactories,
-		// Destroy of the cluster is not working properly. Cluster wasn't full onboarded and it's getting destroyed.
-		// https://castai.atlassian.net/browse/CORE-2868 should solve the issue
-		//CheckDestroy:      testAccCheckAKSClusterDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccAKSNodeConfigurationConfig(rName, clusterName, resourceGroupName, nodeResourceGroupName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", rName),
-					resource.TestCheckResourceAttr(resourceName, "disk_cpu_ratio", "35"),
-					resource.TestCheckResourceAttr(resourceName, "min_disk_size", "122"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.max_pods_per_node", "31"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.aks_image_family", "ubuntu"),
-					resource.TestCheckResourceAttr(resourceName, "eks.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "kops.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "gke.#", "0"),
-				),
-			},
-			{
-				Config: testAccAKSNodeConfigurationUpdated(rName, clusterName, resourceGroupName, nodeResourceGroupName),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(resourceName, "name", rName),
-					resource.TestCheckResourceAttr(resourceName, "disk_cpu_ratio", "0"),
-					resource.TestCheckResourceAttr(resourceName, "min_disk_size", "121"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.max_pods_per_node", "32"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.aks_image_family", "azure-linux"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.ephemeral_os_disk.0.placement", "cacheDisk"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.ephemeral_os_disk.0.cache", "ReadOnly"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.loadbalancers.0.name", "test-lb"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.loadbalancers.0.ip_based_backend_pools.0.name", "test"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.network_security_group", "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/networkSecurityGroups/test-nsg"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.application_security_groups.0", "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/applicationSecurityGroups/test-asg"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.public_ip.0.public_ip_prefix", "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg/providers/Microsoft.Network/publicIPAddresses/test-ip"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.public_ip.0.tags.FirstPartyUsage", "something"),
-					resource.TestCheckResourceAttr(resourceName, "aks.0.public_ip.0.idle_timeout_in_minutes", "10"),
-					resource.TestCheckResourceAttrSet(resourceName, "aks.0.pod_subnet_id"),
-					resource.TestCheckResourceAttr(resourceName, "eks.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "kops.#", "0"),
-					resource.TestCheckResourceAttr(resourceName, "gke.#", "0"),
-				),
-			},
-		},
-		ExternalProviders: map[string]resource.ExternalProvider{
-			"azurerm": {
-				Source:            "hashicorp/azurerm",
-				VersionConstraint: "~> 3.7.0",
-			},
-			"azuread": {
-				Source:            "hashicorp/azuread",
-				VersionConstraint: "~> 2.22.0",
-			},
-		},
-	})
-}
 
 func testAccAKSNodeConfigurationConfig(rName, clusterName, rgName, ngName string) string {
 	return ConfigCompose(testAccAKSClusterConfig(rName, clusterName, rgName, ngName), fmt.Sprintf(`
