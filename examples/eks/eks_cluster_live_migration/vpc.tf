0f864ffb4a591e0605a65287f3b249796c0a3421
Michal Voitiulevic <michal.voitiulevic@cast.ai>
2026-06-16 10:13:36 +0300

    chore: extend subnet ranges

--- examples/eks/eks_cluster_live_migration/vpc.tf
+++ examples/eks/eks_cluster_live_migration/vpc.tf
@@ -9,8 +9,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = data.aws_availability_zones.available.names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+  public_subnets  = ["10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"]
 
   enable_nat_gateway     = true
   single_nat_gateway     = true
