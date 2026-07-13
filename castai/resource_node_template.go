628635365bef9eb36294487a90740900762c4be0
Nikita Kotikov <nikita.kotikov@cast.ai>
2026-07-13 17:46:54 +0300

    feat: remove diff suppress func for spot interruption

--- castai/resource_node_template.go
+++ castai/resource_node_template.go
@@ -289,14 +289,11 @@ func resourceNodeTemplate() *schema.Resource {
 							Description: "Enable/disable spot interruption predictions.",
 						},
 						FieldNodeTemplateSpotInterruptionPredictionsType: {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Default:     "interruption-predictions",
-							Description: "Spot interruption predictions type. Only \"interruption-predictions\" is supported.",
-							Deprecated:  "The value \"aws-rebalance-recommendations\" is deprecated and will be removed in a future major version. Cast AI ML predictions (\"interruption-predictions\") are now used for all spot interruption prediction.",
-							DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
-								return true
-							},
+							Type:             schema.TypeString,
+							Optional:         true,
+							Default:          "interruption-predictions",
+							Description:      "Spot interruption predictions type. Only \"interruption-predictions\" is supported.",
+							Deprecated:       "The value \"aws-rebalance-recommendations\" is deprecated and will be removed in a future major version. Cast AI ML predictions (\"interruption-predictions\") are now used for all spot interruption prediction.",
 							ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"aws-rebalance-recommendations", "interruption-predictions"}, false)),
 						},
 						FieldNodeTemplateMinCpu: {
