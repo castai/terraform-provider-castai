e7af98de4270cda35135e6681609b52eebc8082a
Nikita Kotikov <nikita.kotikov@cast.ai>
2026-07-14 23:15:14 +0300

    fix: improve diff suppress

--- castai/resource_node_template.go
+++ castai/resource_node_template.go
@@ -295,7 +295,10 @@ func resourceNodeTemplate() *schema.Resource {
 							Deprecated:       "The value \"aws-rebalance-recommendations\" is deprecated and will be removed in a future major version. Cast AI ML predictions (\"interruption-predictions\") are now used for all spot interruption prediction.",
 							ValidateDiagFunc: validation.ToDiagFunc(validation.StringInSlice([]string{"aws-rebalance-recommendations", "interruption-predictions"}, false)),
 							DiffSuppressFunc: func(k, oldVal, newVal string, d *schema.ResourceData) bool {
-								return newVal == "" && oldVal == "interruption-predictions"
+								if d.Id() == "" {
+									return false // persist whatever the config resolves to
+								}
+								return true // never diff it
 							},
 							DiffSuppressOnRefresh: true,
 						},
