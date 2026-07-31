c5a1ca94ab3b082ea2e8e57a7a58257712b5c4ce
Daniel Voros <daniel.voros@cast.ai>
2026-07-31 15:17:12 +0200

    chore: update examples to use azurerm 5.x

--- examples/aks/aks_cluster_autoscaler_policies/versions.tf
+++ examples/aks/aks_cluster_autoscaler_policies/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
     azuread = {
       source = "hashicorp/azuread"
