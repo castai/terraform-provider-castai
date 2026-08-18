827533e35a15163652710a9a9c08d8cbae999c57
Laimonas Rastenis <laimonas.rastenis@gmail.com>
2026-08-18 12:46:48 +0300

    chore: is_default node template validation (#784)

--- castai/resource_node_template.go
+++ castai/resource_node_template.go
@@ -157,6 +157,29 @@ func (m nodeSelectorOperatorsSlice) Get(k sdk.K8sSelectorV1Operator) (string, bo
 	return "", false
 }
 
+func resourceNodeTemplateCustomizeDiff(_ context.Context, d *schema.ResourceDiff, _ any) error {
+	name := d.Get(FieldNodeTemplateName).(string)
+	isDefault, isDefaultSet := d.GetOk(FieldNodeTemplateIsDefault)
+	if !isDefaultSet {
+		return nil
+	}
+	return validateNodeTemplateIsDefault(name, isDefault.(bool))
+}
+
+func validateNodeTemplateIsDefault(name string, isDefault bool) error {
+	expected := name == "default-by-castai"
+	if isDefault != expected {
+		return fmt.Errorf(
+			"is_default = %t is invalid for node template %q: it must be %t. "+
+				"The default node template is always 'default-by-castai' - this flag is "+
+				"derived from the template name and cannot be set independently. "+
+				"Remove it from your config; it is computed",
+			isDefault, name, expected,
+		)
+	}
+	return nil
+}
+
 func resourceNodeTemplate() *schema.Resource {
 	supportedArchitectures := []string{ArchAMD64, ArchARM64}
 	supportedOs := []string{OsLinux, OsWindows}
@@ -177,7 +200,8 @@ func resourceNodeTemplate() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			StateContext: nodeTemplateStateImporter,
 		},
-		Description: "CAST AI node template resource to manage node templates",
+		CustomizeDiff: resourceNodeTemplateCustomizeDiff,
+		Description:   "CAST AI node template resource to manage node templates",
 
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(1 * time.Minute),
